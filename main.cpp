07dbb07fa8c1cfe85a5549ded84a053a288b932c
nourkhalfaoui44 <khalfaouinour44@gmail.com>
2025-11-05 00:35:19 +0100

    MARQUE

--- main.cpp
+++ main.cpp
@@ -1,16 +1,23 @@
-#include "smartstudio.h"
-
+#include "marque.h"
+#include "connection.h"
 #include <QApplication>
+#include <QMessageBox>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    smartstudio w;
-
-
-
-
-
+    
+    // Initialiser la connexion à la base de données
+    Connection c;
+    bool test = c.createconnect();
+    
+    if (!test) {
+        QMessageBox::critical(nullptr, "Erreur de connexion", 
+                              "Impossible de se connecter à la base de données!");
+        return -1;
+    }
+    
+    marque w;
     w.show();
     return a.exec();
 }
